9caaa99f1681ccd5b0d7f90b8e71a91516efded5
JONGGON <rlawhdrhs27@gmail.com>
2024-01-21 16:02:54 +0900

    update

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from '@/types';
 
 export const siteConfig: SiteConfig = {
-    name: "Deepfocused Next.js App",
+    name: 'Deepfocused Next.js App',
     description: 'A page introducing deepfocused and what deepfocused has done',
     url: 'https://deepfocused.vercel.app/',
     ogImageUrl: '/images/me.jpg',
